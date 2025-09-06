99e3978ed8183a502618ab55a7ca1c84cfd9b02e
Avni Sen <avnisen@forceget.com>
2025-09-06 15:45:59 +0300

    updated

--- api/get-token.ts
+++ api/get-token.ts
@@ -2,8 +2,14 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 function setCORS(res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Requested-With'
+  );
   res.setHeader('Access-Control-Max-Age', '86400');
 }
 
